2752e5f6887f4d4040b6fbab0f33ded09f26ed37
lucaslangsch <lucas.langsch@gmail.com>
2024-07-24 20:24:50 -0300

    feat: add form validation

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -2,11 +2,21 @@
 
 import Image from "next/image";
 import styles from "./page.module.css";
-import { Button, Card, TextField, Typography } from "@mui/material";
+import { Alert, Button, Card, TextField, Typography } from "@mui/material";
 import swim from "../../../public/swim_01.jpg";
 import { useState } from "react";
 
+export default function Register() {
+const [name, setName] = useState("");
+const [email, setEmail] = useState("");
+const [password, setPassword] = useState("");
+const [messageError, setMessageError] = useState("")
+
 function register(name: string, email: string, password: string) {
+  if (!validateMail(email)) return;
+
+  if (!validatePassword(password)) return;
+
   const body = JSON.stringify({ name, email, password });
   fetch("http://localhost:3001/users/createUser", {
     method: "POST",
@@ -20,10 +30,25 @@ function register(name: string, email: string, password: string) {
     .catch(error => console.error('Error:', error));
 }
 
-export default function Register() {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+function validateMail(email: string) {
+  const regex = /^[a-zA-Z0–9._-]+@[a-zA-Z0–9.-]+\.[a-zA-Z]{2,4}$/;
+  if (!regex.test(email)) {
+    setMessageError("Insira um email válido")
+    return false
+  }
+  setMessageError("")
+  return true
+}
+
+function validatePassword(password: string) {
+  if (password.length < 8) {
+    setMessageError("Insira uma senha com pelo menos 8 caracteres")
+    return false
+  }
+  setMessageError("")
+  return true
+}
+
 
   return (
     <main className={styles.main}>
@@ -42,27 +67,36 @@ export default function Register() {
               Cadastro
             </Typography>
             <TextField
-              id="standard-basic"
+              id="standard-name"
               label="Nome"
+              required
               variant="standard"
               value={name}
-              onChange={(e) => setName(e.target.value)} />
+              onChange={(e) => setName(e.target.value)}
+            />
             <TextField
-              id="standard-basic"
+              id="standard-email"
               label="Email"
+              required
               variant="standard"
               value={email}
               onChange={(e) => setEmail(e.target.value)} />
             <TextField
               id="standard-password-input"
               label="Senha"
+              required
               type="password"
+              
               autoComplete="current-password"
               variant="standard"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
             />
             <Button variant="contained" type="submit">Cadastrar</Button>
+            {
+              messageError.length > 1 && 
+              <Alert severity="warning">{ messageError }</Alert>
+            }
           </form>
         </Card>
       </div>
