f071edd50ed1c183bdc87474c6235af8a6954df4
lucaslangsch <lucas.langsch@gmail.com>
2024-07-29 19:12:57 -0300

    remove middleware api

--- src/middleware.ts
+++ src/middleware.ts
@@ -18,5 +18,5 @@ export function middleware(request: NextRequest) {
 }
  
 export const config = {
-  matcher: ['/((?!api/auth|_next/static|_next/image|.*\\.png$).*)'],
+  matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
 }
