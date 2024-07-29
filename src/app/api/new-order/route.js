17b5f5b7c0056dc3b01bf7f9306d7d49810b7f0e
lucaslangsch <lucas.langsch@gmail.com>
2024-07-29 19:36:38 -0300

    fix: endpoint test POST

--- src/app/api/new-order/route.js
+++ src/app/api/new-order/route.js
@@ -1,13 +1,33 @@
 import { sql } from '@vercel/postgres';
 import { NextResponse } from 'next/server';
- 
+
 export async function POST(request) {
   const body = JSON.stringify(request.body);
   try {
     const result =
       await sql`INSERT INTO pets1 (name) VALUES (${body});`;
-    return NextResponse.json({ result }, { status: 200 });
+    return NextResponse.json(
+      { result },
+      {
+        status: 200,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+          'Access-Control-Allow-Headers': 'Content-Type',
+        },
+      }
+    );
   } catch (error) {
-    return NextResponse.json({ error }, { status: 500 });
+    return NextResponse.json(
+      { error },
+      {
+        status: 500,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+          'Access-Control-Allow-Headers': 'Content-Type',
+        },
+      }
+    );
   }
 }
