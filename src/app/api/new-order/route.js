24891b86a09bb6f7361d2d45592a9cae30be0519
lucaslangsch <lucas.langsch@gmail.com>
2024-07-29 19:14:35 -0300

    fix: endpoint test POST

--- src/app/api/new-order/route.js
+++ src/app/api/new-order/route.js
@@ -1,7 +1,7 @@
 import { sql } from '@vercel/postgres';
 import { NextResponse } from 'next/server';
  
-export async function GET(request) {
+export async function POST(request) {
   const body = JSON.stringify(request.body);
   try {
     const result =
